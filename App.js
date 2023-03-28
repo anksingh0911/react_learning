9e979d4d9f52261997c34fa45e00e26941c1db18
anksingh0911 <anksingh0911@gmail.com>
2023-03-28 22:23:43 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement(
+'h1',
+{
+  id:'heading', 
+  xyz:'anything'
+}, 
+'Hello World in javaScript Learning React'
+);
+console.log(heading,'heading');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
