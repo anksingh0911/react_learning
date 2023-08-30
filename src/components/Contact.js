5a9d1319cdc28955a564ba0ae87e5a401e7cfb79
anksingh0911 <anksingh0911@gmail.com>
2023-08-30 10:13:00 +0530

    feat: set up for test cases, add babel config file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,11 @@
 const Contact =  ()=>{
   return(
-    <>
-      Contact page
-    </>
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">Contact Us</h2>
+      <input type='text' className="p-1 border-gray-950 border-[1px] rounded-md  mr-2" placeholder="Name"/>
+      <input type='text' className="p-1 border-gray-950 border-[1px] rounded-md mr-2" placeholder="Query"/>
+      <button className="bg-green-500 py-1 px-2 rounded-md">Submit</button>
+    </div>
   )
 }
 export default Contact;
